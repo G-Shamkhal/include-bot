77335375654f75fe950532fb10ffa4afe8729687
Agresor <agresor_2001@mail.ru>
2021-03-09 19:25:46 +0500

    init

--- bot.js
+++ bot.js
@@ -1,19 +1,13 @@
 const Discord = require('discord.js'); // Подключаем библиотеку discord.js
 const robot = new Discord.Client(); // Объявляем, что robot - бот
 const comms = require("./comms.js"); // Подключаем файл с командами для бота
-const fs = require('fs'); // Подключаем родной модуль файловой системы node.js  
-const ytdl = require('ytdl-core'); // Подключаем библиотеку ytdl-core
 let config = require('./config.json'); // Подключаем файл с конфигруацией
 
 const prefix = config.prefix; // «Вытаскиваем» из него префикс
 const token = process.env.token;
 
-<<<<<<< HEAD
-
-robot.on("ready", function() {
-=======
 robot.on("ready", function () {
->>>>>>> ee0498ceb81be0d85f60e630ed12604389d3122e
+
   /* При успешном запуске, в консоли появится сообщение «[Имя бота] запустился!» */
   console.log(robot.user.username + " запустился!");
 });
@@ -24,11 +18,8 @@ robot.on('message', (msg) => { // Реагирование на сообщени
   if (msg.author.username != robot.user.username && msg.author.discriminator != robot.user.discriminator) {
     var comm = msg.content.trim() + " ";
     var comm_name = comm.slice(0, comm.indexOf(" "));
-<<<<<<< HEAD
-    var messArr = comm.split(" ");
-=======
+
     var args = comm.split(" ");
->>>>>>> ee0498ceb81be0d85f60e630ed12604389d3122e
 
     for (comm_count in comms.comms) {
       var comm2 = prefix + comms.comms[comm_count].name;
