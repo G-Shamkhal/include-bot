2bee989a911ed17122a5d045ffad2cbf62cec295
Shamkhal <g_shamkhal_2001@mail.ru>
2021-08-30 08:46:51 +0500

    Add GitHub code

--- modules.py
+++ modules.py
@@ -2,10 +2,15 @@ from config import config
 import discord
 from discord import client
 from discord.ext import commands
+from discord.utils import get
 import json
 import os
 import random
 import asyncio
+from async_timeout import timeout
+import functools
+import itertools
+import math
 import sys
 import requests
 import inspect
@@ -14,11 +19,6 @@ from googletrans import Translator
 from google_trans_new import google_translator
 from google.cloud import translate
 import youtube_dl
-
-
-
-
-
 
 import functions
 
