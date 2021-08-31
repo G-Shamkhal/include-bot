579d593637feb00333cdc6b12ea4804aa07bbef3
Shamkhal <g_shamkhal_2001@mail.ru>
2021-08-31 09:09:15 +0500

    Bot v1.124 adaptation under Heroky

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-from config import config
+#from config import config
 import discord
 from discord import client
 from discord.ext import commands
@@ -21,16 +21,17 @@ from googletrans import Translator
 from google_trans_new import google_translator
 from google.cloud import translate
 import youtube_dl
+from ffmpy import FFmpeg
 
-#token = os.environ['token']
-#prefix = os.environ['prefix']
-#searchID = os.environ['searchID']
-#api_key = os.environ['api_key']
+token = os.environ['token']
+prefix = os.environ['prefix']
+searchID = os.environ['searchID']
+api_key = os.environ['api_key']
 
-token = config['token']
-prefix = config['prefix']
-searchID = config['searchID']
-api_key = config['api_key']
+#token = config['token']
+#prefix = config['prefix']
+#searchID = config['searchID']
+#api_key = config['api_key']
 
 import functions
 
